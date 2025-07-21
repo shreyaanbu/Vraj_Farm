cd5f074ad9c9dac81bc862f5f4679c58b3a78f92
shreyaanbu <shreyaanbu@gmail.com>
2025-07-21 18:07:40 +0530

    Add contacts footer

---
+++ frontend/src/assets/components/ContactFooter.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const ContactFooter = () => {
+  return (
+    <div className='bg-white px-10 pb-12 pt-10 flex flex-wrap gap-30 text-lg text-[var(--green)]'>
+      <div>
+        <h2 className = 'font-bold text-xl'>Contact Us:</h2>
+        <p>Address: </p>
+        <p>Email: </p>
+        <p>Phone No.: </p>
+      </div>
+      <div>
+        <h2 className = 'font-bold text-xl'>Social Media:</h2>
+        <a href='https://www.instagram.com/vraj_farm_/' target="_blank">Instagram</a>
+      </div>
+    </div>
+  )
+}
+
+export default ContactFooter
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../assets/components/Hero'
+import ContactFooter from '../assets/components/ContactFooter'
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <ContactFooter />
     </div>
   )
 }
