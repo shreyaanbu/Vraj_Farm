d42ddc21920aac10fb486f4ac03cc990b3faf554
Shreya Anbu <shreyaanbu@gmail.com>
2026-06-28 14:48:06 +0530

    Updated mongodb.js

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,10 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const connectDB = async() => {
-    mongoose.connection.on('connected', () => {
-        console.log('DB Connected');
-    })
-    await mongoose.connect(`${process.env.MONGODB_URI}/vraj-kesar-farm`)
-}
+const connectDB = async () => {
+    mongoose.connection.on("connected", () => {
+        console.log("DB Connected");
+    });
+
+    return mongoose.connect(
+        `${process.env.MONGODB_URI}/vraj-kesar-farm`,
+        {
+            serverSelectionTimeoutMS: 10000,
+        }
+    );
+};
 
 export default connectDB;
